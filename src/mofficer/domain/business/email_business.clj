31afa8ea69f324417b8e991da1742834ff1902b7
Julian Espinel <julianespinel@gmail.com>
2014-09-15 18:57:37 -0500

    Remove message queue. Move the send mail logic to email_business.clj file.

--- src/mofficer/domain/business/email_business.clj
+++ src/mofficer/domain/business/email_business.clj
@@ -1,8 +1,22 @@
 (ns mofficer.domain.business.email-business
-  (:require [mofficer.infrastructure.mq.rabbit-handler :as rabbit-handler]))
+  (:require [cheshire.core :as ches]
+            [postal.core :as postal]
+            [mofficer.domain.business.user-config-business :as user-config-business]))
 
-(def email-queue-name "mofficer_email-queue")
+(def utf-8 "UTF-8")
 
-(defn add-message-to-email-queue [user-config email-info]
-  (let [message-as-clojure-map {:user-config user-config :email-info email-info}]
-    (rabbit-handler/add-message-to-queue email-queue-name message-as-clojure-map)))
+(defn get-message [user-config email-info] {:host (:emailHost user-config), :port (:emailPort user-config), :user (:senderUsername user-config), :pass (:senderPassword user-config)}
+                                           {:from (:senderEmail email-info), :to (:recipients email-info), :subject (:subject email-info), :body (:body email-info)})
+
+(defn send-email-result [user-config email-info] 
+  (let [postal-answer (postal/send-message (get-message user-config email-info))]
+    (if (= 0 (:code postal-answer)) true false)))
+
+
+(defn send-email [user-config email-info] 
+  (println "Sending email to: " (:recipients email-info) " with subject: " (:subject email-info) " and message: " (:body email-info))
+  (let [send-email (:senderEmail email-info)
+        user-config (user-config-business/get-user-config-by-email sender-email)]
+    (if (= sender-id (:senderId user-config))
+      send-email-result
+      false)))
--- src/mofficer/service/email_resource.clj
+++ src/mofficer/service/email_resource.clj
@@ -3,23 +3,15 @@
   (:require [ring.util.response :as ring-resp]
             [mofficer.infrastructure.datastructures.either]
             [mofficer.domain.business.email-business :as email-business]
-            [mofficer.domain.business.user-config-business :as user-config-business]
             [mofficer.infrastructure.translators.email-info-translator :as email-info-translator])
   (:import [mofficer.infrastructure.datastructures.either Either]))
 
-(defn- queue-email-request [sender-id email-info]
-  (let [sender-email (:senderEmail email-info)
-        user-config (user-config-business/get-user-config-by-email sender-email)]
-    (if (= sender-id (:senderId user-config))
-      (email-business/add-message-to-email-queue user-config email-info)
-      (Either. "The user who is calling the service and the email owner are not the same" nil))))
-
 (defn send-email [sender-id email-info-map]
-  (let [email-info (email-info-translator/get-email-info-from-map email-info-map)
-        queue-email-either (queue-email-request sender-id email-info-map)]
-    (if-not (nil? (:successAnswer queue-email-either))
-      {:status 202 :body (:successAnswer queue-email-either)}
-      {:status 500 :body (:errorMessage queue-email-either)})))
+  (let [email-info (email-info-translator/get-email-info-from-map email-info-map)]
+    (if-not (nil? (:senderEmail email-info))
+      (do (future (email-business/send-email sender-id email-info))
+          {:status 202})
+      {:status 500})))
 
 (defroutes email-api
   (context "/emails/:sender-id" [sender-id]
