a6d4ce16022f1915363a0b37b297dbc161ec1b9c
Julian Espinel <julianespinel@gmail.com>
2014-09-16 11:33:01 -0500

    Fix google smpt bug. Code refactor in order to make it more readable.

--- src/mofficer/domain/business/email_business.clj
+++ src/mofficer/domain/business/email_business.clj
@@ -3,17 +3,24 @@
             [postal.core :as postal]
             [mofficer.domain.business.user-config-business :as user-config-business]))
 
-(def utf-8 "UTF-8")
 
-(defn- send-email-result [user-config email-info] 
-  (let [postal-answer (postal/send-message {:host (:emailHost user-config), :port (:emailPort user-config), :user (:senderUsername user-config), :pass (:senderPassword user-config)}
-                                           {:from (:senderEmail email-info), :to (:recipients email-info), :subject (:subject email-info), :body (:body email-info)})]
-    (if (= 0 (:code postal-answer)) true false)))
+(defn get-mail-connection [user-config] {:host (:emailHost user-config)
+                                         :ssl true
+                                         :user (:senderUsername user-config)
+                                         :pass (:senderPassword user-config)})
+
+(defn get-mail-message [email-info] {:from (:senderEmail email-info)
+                                     :to (:recipients email-info)
+                                     :subject (:subject email-info)
+                                     :body (:body email-info)})
+
+(defn- send-email-and-get-result [user-config email-info] 
+  (let [mail-connection (get-mail-connection user-config)
+        mail-message (get-mail-message email-info)]
+    (postal/send-message mail-connection mail-message)))
 
 (defn send-email [sender-id email-info] 
   (println "Sending email to: " (:recipients email-info) " with subject: " (:subject email-info) " and message: " (:body email-info))
   (let [sender-email (:senderEmail email-info)
         user-config (user-config-business/get-user-config-by-email sender-email)]
-    (if (= sender-id (:senderId user-config))
-      (send-email-result user-config email-info)
-      false)))
+    (send-email-and-get-result user-config email-info)))
--- src/mofficer/service/email_resource.clj
+++ src/mofficer/service/email_resource.clj
@@ -7,11 +7,11 @@
   (:import [mofficer.infrastructure.datastructures.either Either]))
 
 (defn send-email [sender-id email-info-map]
-  (let [email-info (email-info-translator/get-email-info-from-map email-info-map)]
+  (let [email-info (email-info-translator/get-email-info-from-map email-info-map)
+        send-future (future (email-business/send-email sender-id email-info))]
     (if-not (nil? (:senderEmail email-info))
-      (do (email-business/send-email sender-id email-info)
-          {:status 202})
-      {:status 500})))
+      {:status 202 :body @send-future}
+      {:status 500 :body @send-future})))
 
 (defroutes email-api
   (context "/emails/:sender-id" [sender-id]
