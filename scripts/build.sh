f1cadfb5ade18819e22284b6d243567d7a7994b1
Julian Espinel <julianespinel@gmail.com>
2015-06-05 16:15:41 -0500

    Bug fix: correct the build and start scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,2 @@
+cd ..
 lein uberjar
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,2 @@
-java -jar -Xms75m -Xmx75m -XX:MaxPermSize=40m mofficer/target/mofficer-1.0-SNAPSHOT-standalone.jar &
+cd ..
+java -jar -Xms75m -Xmx75m -XX:MaxPermSize=40m target/mofficer-1.0-SNAPSHOT-standalone.jar &
